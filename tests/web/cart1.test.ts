3bcaa55be2b22c67db1ee28e89fa4aa243668292
Renju Gladen <renju.gladen@mdgroup.com>
2026-03-03 23:49:30 +0000

    tag

--- tests/web/cart1.test.ts
+++ tests/web/cart1.test.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@fixtures/base.fixture';
 import { TAGS } from '@constants/tags';
 
-test('Add item to cart1', { tag: [TAGS.SMOKE, TAGS.AUTH] }, async ({ webCataloguePage, webProductPage, t, routes }) => {
-  await webCataloguePage.open(routes.HOME);
-  await webCataloguePage.navBar.verifyUserIsLoggedIn();
-  await webCataloguePage.selectProduct('Combination Pliers');
-  await webProductPage.addToCart();
-  await webProductPage.verifyToasterMessage(t('TOASTER.PRODUCT_ADDED'));
-  await expect(webProductPage.page).toHaveURL(/.*product/);
-});
+test(
+  'Add item to cart1',
+  { tag: [TAGS.REGRESSION, TAGS.AUTH] },
+  async ({ webCataloguePage, webProductPage, t, routes }) => {
+    await webCataloguePage.open(routes.HOME);
+    await webCataloguePage.navBar.verifyUserIsLoggedIn();
+    await webCataloguePage.selectProduct('Combination Pliers');
+    await webProductPage.addToCart();
+    await webProductPage.verifyToasterMessage(t('TOASTER.PRODUCT_ADDED'));
+    await expect(webProductPage.page).toHaveURL(/.*product/);
+  }
+);
